ae245444a174d7e9d10aff240179e855839fa06a
drevnyy <drevnyy@gmail.com>
2015-06-14 08:56:01 +0200

    initial commit

---
+++ MagiSearchEngine/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MagiSearchEngine.Startup))]
+namespace MagiSearchEngine
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
